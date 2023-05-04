e52e08f4513ccafbecfc95a2b2898d6c90428ef2
Sebastian Seiss <sebastian.seiss@uni-weimar.de>
2023-05-05 00:01:36 +0200

    publication test

---
+++ 08_Puclication/230504_Veröffentlichung/GenerateHTMLdoc.py
@@ -0,0 +1,18 @@
+#from pylode import OntDoc
+from pylode import OntDoc, MakeDocco, APP_DIR
+from pathlib import Path
+from os.path import join, dirname, abspath
+
+####ALTE VERSION####
+#od = OntDoc(Path(__file__).parent / "OCQA_03.ttl")
+## or save HTML to a file
+#od.make_html(destination="OCQA_03_DOC.html")
+
+####OBPA VERSION ####
+TESTS_DIR = dirname(abspath(__file__))
+print(TESTS_DIR)
+print(join(TESTS_DIR, "OCQA_03.ttl"))
+#od=OntDoc(default_language="en",source_info=input_rdf, g=input_rdf)
+h = MakeDocco(input_data_file=join(TESTS_DIR, "OCQA_03.ttl"))
+# generate the HTML doc
+h.document(destination=join(TESTS_DIR, "OCQA_03_DOC.html"))
